dc86396781b06b83b0c222e917fe0181a5f1a84a
0613ms <0613ms@naver.com>
2025-07-23 17:49:13 +0900

    0723 실습

---
+++ 0723run.py
@@ -0,0 +1,67 @@
+# def my_multi(number_1, number_2):
+#     return number_1 * number_2
+# result_1 = my_multi(2, 3)
+# print(result_1)
+# # my_multi(2, 3) 결과 : 6
+# # 함수를 수정하고 호출 결과를 result_1 변수에 할당하여 출력하시오.
+
+
+# def is_negative(number):
+#     if number <= 0:
+#         return True
+#     else:
+#         return False
+# result_2 = is_negative(3)
+# print(result_2)
+# # is_negative(3) 결과 : False
+# # 함수를 수정하고 호출 결과를 result_2 변수에 할당하여 출력하시오.
+
+# def default_arg_func(default = '기본 값'):
+#     return default
+
+# result_3 = default_arg_func()
+# result_4 = default_arg_func('다른 값')
+
+# print(result_3)
+# print(result_4)
+
+
+
+'''
+number_of_people = 0
+
+
+def increase_user():
+    global number_of_people 
+    number_of_people += 1
+
+
+increase_user()
+print("현재 가입 된 유저 수 :", number_of_people)
+'''
+
+# # 아래 함수를 수정하시오.
+# def add_numbers(num_1, num_2):
+#     return num_1 + num_2
+# result = add_numbers(3, 5)
+# print(result)
+# # 수정한 add_numbers() 함수를 호출하시오.
+
+'''
+# 1. 절댓값을 반환하는 함수 abs를 사용하여 아래 변수에 담긴 값의 절댓값을 출력하시오.
+negative = -3
+print(abs(negative))
+
+# 2. 아래 변수에 담긴 값의 boolean 값을 출력하시오.
+empty_list = []
+print(bool(empty_list))
+
+# 3. 주어진 리스트가 가진 모든 값을 더한 결과를 출력하시오.
+my_list = [1, 2, 3, 4, 5]
+print(sum(my_list))
+
+# 4. 주어진 정렬을 오름차순으로 정렬한 결과를 출력하시오.
+unsorted_list = ['하', '교', '캅', '의', '지', '가']
+print(sorted(unsorted_list, reverse=False))
+'''
+
